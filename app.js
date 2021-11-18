24f04425bddddc5f7daec659ceca5a624f95c795
Andrei Ghervan <andrei.ghervan8@gmail.com>
2021-11-18 14:10:14 +0200

    make it better

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ const errorHandler = require('./helpers/error-handler');
 app.use(cors());
 app.options('*', cors());
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 //middleware
 app.use(express.json());
 app.use(morgan('tiny'));
