fd9877bce663be1a75ae1c6c0acf9d85adc0c1c4
andreighervan <andrei.ghervan8@gmail.com>
2023-02-03 16:22:35 +0200

    aw

--- routes/posts.js
+++ routes/posts.js
@@ -14,14 +14,14 @@ const FILE_TYPE_MAP = {
 };
 
 // SET STORAGE
-var storage = multer.diskStorage({
+/*var storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'uploads')
     },
     filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now())
     }
-})
+})*/
 
 //const uploadOptions = multer({ storage: storage });
 
